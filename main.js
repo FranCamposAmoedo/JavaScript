a18e74add85dab9b47a1cfc3b5a5b0476d8454cf
FranciscoCampos <francisco.campos.amoedo@gmail.com>
2021-09-28 19:31:51 -0300

    Desafio1

---
+++ main.js
@@ -0,0 +1,8 @@
+//VARIABLES
+//Le pido al usuario nombre, apellido y edad
+let nombre = prompt("¿Cuál es tu nombre?");
+let apellido = prompt("¿Cuál es tu apellido?");
+let edad = prompt("¿Cuántos años tenés?");
+
+//Muestro los datos en una ventana
+alert("Hola " + nombre + " " + apellido + ". Tenés " + edad + " años de edad");
